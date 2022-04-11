ca3edee5e8252642b9084f8ec3c2cfc9729e1a47
laney522 <pm72222@gmail.com>
2022-04-11 16:51:10 +0900

    updated

--- api/index.js
+++ api/index.js
@@ -5,6 +5,9 @@ const mongoose = require("mongoose");
 const authRoute = require("./routes/auth");
 const userRoute = require("./routes/users");
 const postRoute = require("./routes/posts");
+const categoryRoute = require("./routes/categories");
+const multer = require("multer");
+const { db } = require('./models/User');
 
 dotenv.config();
 app.use(express.json());
@@ -18,9 +21,24 @@ mongoose
   .then(console.log("Connected to MongoDB"))
   .catch((err) => console.log(err));
 
-  app.use("/api/auth", authRoute);
-  app.use("/api/users", userRoute);
-  app.use("/api/posts", postRoute);
+  const storage = multer.diskStorage({
+    destination:(req,file,cb) => {
+      cb(null,"images")
+    }, 
+    filename:(req, file, cb) => {
+      cb(null, req.body.name);
+    },
+  })
+
+  const upload = multer({storage:storage});
+  app.post("/api/upload", upload.single("file"), (req, res) => {
+    res.status(200).json("File has been upload");
+  });
+
+app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
+app.use("/api/posts", postRoute);
+app.use("/api/categories", categoryRoute);
 
 app.listen("5000", () => {
   console.log("Backend is running.");
--- api/routes/posts.js
+++ api/routes/posts.js
@@ -4,46 +4,87 @@ const Post = require("../models/Post");
 
 //CREATE POST
 router.post("/", async (req, res) => {
-    const newPost = new Post(req.body);
-    try {
-      const savedPost = await newPost.save();
-      res.status(200).json(savedPost);
-    } catch (err) {
-      res.status(500).json(err);
-    }
+  const newPost = new Post(req.body);
+  try {
+    const savedPost = await newPost.save();
+    res.status(200).json(savedPost);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
-
-
-
 
 //UPDATE POST
 router.put("/:id", async (req, res) => {
-  if(req.body.userId === req.params.id ){
-    try{
-      const user = await User.findById(req.params.id);
+  try {
+    const post = await Post.findById(req.params.id);
+    if (post.username === req.body.username) {
       try {
-        await Post.deleteMany({ username: user.username });        
-        await User.findByIdAndRemove(req.params.id);
-        res.status(200).json("User has been deleted...");
+        const updatedPost = await Post.findByIdAndUpdate(
+          req.params.id,
+          {
+            $set: req.body,
+          },
+          { new: true }
+        );
+        res.status(200).json(updatedPost);
       } catch (err) {
         res.status(500).json(err);
       }
-    } catch (err) {
-      res.status(404).json("User not found!");
+    } else {
+      res.status(401).json("You can update only your post!");
     }
-  } else {
-    res.status(401).json("You can delete only your account!");
+  } catch (err) {
+    res.status(500).json(err);
   }
 });
 
 //DELETE POST
+router.delete("/:id", async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if (post.username === req.body.username) {
+      try {
+        await post.delete();
+        res.status(200).json("Post has been deleted...");
+      } catch (err) {
+        res.status(500).json(err);
+      }
+    } else {
+      res.status(401).json("You can delete only your post!");
+    }
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 //GET POST
 router.get("/:id", async (req, res) => {
   try {
-    const user = await User.findById(req.params.id);
-    const { password, ...others } = user._doc;
-    res.status(200).json(others);
+    const post = await Post.findById(req.params.id);
+    res.status(200).json(post);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+//GET ALL POSTS
+router.get("/", async (req, res) => {
+  const username = req.query.user;
+  const catName = req.query.cat;
+  try {
+    let posts;
+    if(username){
+      posts = await Post.find({username})
+    } else if (catName) {
+      posts = await Post.find({
+        categories: {
+          $in: [catName],
+        },
+      });
+    } else {
+      posts = await Post.find();
+    }
+    res.status(200).json(posts);
   } catch (err) {
     res.status(500).json(err);
   }
