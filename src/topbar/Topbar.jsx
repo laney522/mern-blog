7e8f9f65f7e8c34c2a6536a214b25c58cb6843c0
laney522 <pm72222@gmail.com>
2022-06-19 21:19:47 +0900

    font-awesome-facebook

--- src/topbar/Topbar.jsx
+++ src/topbar/Topbar.jsx
@@ -3,7 +3,11 @@ import "./topbar.css"
 export default function Topbar() {
   return (
     <div className="top">
-      Topbar
+      <div className="topLeft">
+        <i class="fa-brands fa-facebook-square"></i>
+      </div>
+      <div className="topCenter">c</div>
+      <div className="topRight">r</div>
     </div>
 
   )
