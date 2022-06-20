e574c164e414e11f843ef0b4b85ee0e29cf1cec9
laney522 <pm72222@gmail.com>
2022-06-20 22:20:19 +0900

    square link img update

--- src/topbar/Topbar.jsx
+++ src/topbar/Topbar.jsx
@@ -5,8 +5,19 @@ export default function Topbar() {
     <div className="top">
       <div className="topLeft">
         <i class="fa-brands fa-facebook-square"></i>
+        <i class="fa-brands fa-twitter-square"></i>
+        <i class="fa-brands fa-pinterest-square"></i>
+        <i class="fa-brands fa-instagram-square"></i>
       </div>
-      <div className="topCenter">c</div>
+      <div className="topCenter">
+        <ul className="topList">
+          <li className="topListItem">HOME</li>
+          <li className="topListItem">ABOUT</li>
+          <li className="topListItem">CONTACT</li>
+          <li className="topListItem">WRITE</li>
+          <li className="topListItem">LOGOUT</li>
+        </ul>
+      </div>
       <div className="topRight">r</div>
     </div>
 
