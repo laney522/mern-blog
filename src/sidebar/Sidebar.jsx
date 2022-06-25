c02e62fc218096e8d4b0aaf1964de74dabb3cb71
laney522 <pm72222@gmail.com>
2022-06-25 21:27:09 +0900

    sidebar css

--- src/sidebar/Sidebar.jsx
+++ src/sidebar/Sidebar.jsx
@@ -3,7 +3,36 @@ import "./sidebar.css"
 export default function Sidebar() {
   return (
     <div className='sidebar'>
-      sidebar
+      <div className="sidebarItem">
+        <span className="sisdebarTitle">ABOUT ME</span>
+        <img
+          src="https://images.pexels.com/photos/8385886/pexels-photo-8385886.jpeg?auto=compress&cs=tinysrgb&w=600"
+          alt=""
+        />
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum
+        </p>
+      </div>
+      <div className="sidebarItem">
+        <span className="sidebarTitle">CATEGORIES</span>
+        <ul className="sidebarList">
+          <li className="sidebarListItem">Life</li>
+          <li className="sidebarListItem">Music</li>
+          <li className="sidebarListItem">Style</li>
+          <li className="sidebarListItem">Sport</li>
+          <li className="sidebarListItem">Tech</li>
+          <li className="sidebarListItem">Cinema</li>
+        </ul>
+      </div>
+      <div className="sidebarItem">
+        <span className="sidebarTitle">FOLLOW US</span>
+        <div className="sidebarSocial">
+          <i className="sidebarIcon fab fa-brands fa-facebook-square"></i>
+          <i className="sidebarIcon fab fa-brands fa-twitter-square"></i>
+          <i className="sidebarIcon fab fa-brands fa-pinterest-square"></i>
+          <i className="sidebarIcon fab fa-brands fa-instagram-square"></i>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
