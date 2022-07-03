6474cbc136335654d7c1b8228accfb837a26cdd5
laney522 <pm72222@gmail.com>
2022-07-03 15:03:44 +0900

    add settings page

--- src/pages/settings/Settings.jsx
+++ src/pages/settings/Settings.jsx
@@ -7,7 +7,20 @@ export default function Settings() {
       <div className="settingsWrapper">
         <div className="settingsTitle">
           <span className="settingsUpdateTitle">Update Your Account</span>
+          <span className="settingsUpdateTitle">Delete Account</span>
         </div>
+        <form className="settingsForm">
+          <label>Profile Picture</label>
+          <div className="settingsPP">
+            <img
+              src="https://images.pexels.com/photos/4072840/pexels-photo-4072840.jpeg?auto=compress&cs=tinysrgb&w=600"
+              alt="" />
+          </div>
+          <label htmlFor="fileInput">
+            <i className="settingsPPIcon fa-solid fa-circle-user"></i>
+          </label>
+          <input type="file" id="fileInput" style={{ display: "none" }} />
+        </form>
       </div>
       <Sidebar />
     </div>
