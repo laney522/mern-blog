af90957dd4d4bd495ec8010de9e6827fc5141157
laney522 <pm72222@gmail.com>
2022-07-04 22:14:31 +0900

    update settings page

--- src/pages/settings/Settings.jsx
+++ src/pages/settings/Settings.jsx
@@ -7,7 +7,7 @@ export default function Settings() {
       <div className="settingsWrapper">
         <div className="settingsTitle">
           <span className="settingsUpdateTitle">Update Your Account</span>
-          <span className="settingsUpdateTitle">Delete Account</span>
+          <span className="settingsDeleteTitle">Delete Account</span>
         </div>
         <form className="settingsForm">
           <label>Profile Picture</label>
@@ -15,11 +15,18 @@ export default function Settings() {
             <img
               src="https://images.pexels.com/photos/4072840/pexels-photo-4072840.jpeg?auto=compress&cs=tinysrgb&w=600"
               alt="" />
+            <label htmlFor="fileInput">
+              <i className="settingsPPIcon fa-solid fa-circle-user"></i>
+            </label>
+            <input type="file" id="fileInput" style={{ display: "none" }} />
           </div>
-          <label htmlFor="fileInput">
-            <i className="settingsPPIcon fa-solid fa-circle-user"></i>
-          </label>
-          <input type="file" id="fileInput" style={{ display: "none" }} />
+          <label>Username</label>
+          <input type="text" placeholder='laney' />
+          <label>Email</label>
+          <input type="email" placeholder='laney@gmail.com' />
+          <label>Password</label>
+          <input type="password" />
+          <button className="settingsSubmit">Update</button>
         </form>
       </div>
       <Sidebar />
