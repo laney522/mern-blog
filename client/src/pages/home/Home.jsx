99d92ff301d91b5a9b19f5039def0032ac11cb46
laney522 <pm72222@gmail.com>
2022-07-14 23:24:03 +0900

    add axios

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -1,9 +1,19 @@
+import { useEffect } from 'react';
+import { useState } from 'react';
 import Header from '../../components/header/Header';
 import Posts from '../../components/posts/Posts';
 import Sidebar from '../../components/sidebar/Sidebar';
 import "./home.css";
+import axios from "axios";
 
 export default function Home() {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      axios.get("")
+    }
+  }, [])
   return (
     <>
       <Header />
