bbf3ee6e20788505429e916771727110aebc4693
laney522 <pm72222@gmail.com>
2022-07-16 22:58:25 +0900

    yarn add axios

--- client/src/components/post/Post.jsx
+++ client/src/components/post/Post.jsx
@@ -1,29 +1,30 @@
 import "./post.css";
+import {Link} from "react-router-dom"
 
 export default function Post({ post }) {
   return (
     <div className='post'>
-      <img
-        className="postImg"
-        src="https://images.pexels.com/photos/10757870/pexels-photo-10757870.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
-        alt=""
-      />
+      {post.photo && (
+          <img
+          className="postImg"
+          src={post.photo}
+          alt=""
+          //https://images.pexels.com/photos/10757870/pexels-photo-10757870.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load
+        />
+      )}
       <div className="postInfo">
         <div className="postCats">
-          <span className="postCat">Music</span>
-          <span className="postCat">Life</span>
+          {post.categories.map((c) => (
+            <span className="postCat">{c.name}</span>
+          ))}
         </div>
-        <span className="postTitle">
-          {post.title}
-        </span>
+        <Link to={`/post/${post._id}`}>
+          <span className="postTitle">{post.title}</span>
+        </Link>
         <hr />
-        <span className="postDate">1 hour ago</span>
+        <span className="postDate">{new Date(post.createdAt).toDateString}</span>
       </div>
-      <p className="postDesc">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas venenatis aliquet ante ut lobortis. Pellentesque habitant morbi tristique senectus et.
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas venenatis aliquet ante ut lobortis. Pellentesque habitant morbi tristique senectus et.
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas venenatis aliquet ante ut lobortis. Pellentesque habitant morbi tristique senectus et.
-      </p>
+      <p className="postDesc">{post.desc}</p>
     </div>
   )
 }
