ca1f2b6065243b2f7cbe7ae1c958a03ed80a7698
laney522 <pm72222@gmail.com>
2022-07-24 21:57:31 +0900

    update auth.js

--- api/index.js
+++ api/index.js
@@ -11,7 +11,7 @@ mongoose.connect(process.env.MONGO_URL, {
 
 })
   .then(console.log("Connected to MongoDB"))
-  .catch(err => console.log(err));
+  .catch((err) => console.log(err));
 
 app.use("/api/auth", authRoute);
 
