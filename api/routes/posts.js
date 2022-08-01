c3d853e2ef79ec6ca00ccc057253d38e9fa131a7
laney522 <pm72222@gmail.com>
2022-08-01 21:15:59 +0900

    test1

--- api/routes/posts.js
+++ api/routes/posts.js
@@ -68,10 +68,22 @@ router.get("/:id", async (req, res) => {
 });
 
 //GET ALL POSTS
-router.get("/:id", async (req, res) => {
+router.get("/", async (req, res) => {
+  const username = req.query.user;
+  const catName = req.query.cat;
   try {
-    const post = await Post.findById(req.params.id);
-    res.status(200).json(post);
+    let posts;
+    if (username) {
+      posts = await Post.find({ username })
+    } else if (catName) {
+      posts = await Post.find({
+        categories: {
+          $in: [catName]
+        }})
+    } else{
+      posts = await Post.find();
+    }
+    res.status(200).json(posts);
   } catch (err) {
     res.status(500).json(err)
   }
