e609dab6242be56b00cac2324b3879edf32ec490
laney522 <pm72222@gmail.com>
2022-08-09 20:57:02 +0900

    edited singplePost.jsx

--- client/src/components/singlePost/SinglePost.jsx
+++ client/src/components/singlePost/SinglePost.jsx
@@ -1,17 +1,19 @@
 import axios from "axios";
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useLocation } from 'react-router-dom'
 import "./singlePost.css"
 
 export default function SinglePost() {
   const location = useLocation()
   const path = location.pathname.split("/")[2];
+  const [post, setPost] = useState({})
 
   useEffect(() => {
     const getPost = async () => {
-      const res = axios.get("/posts/" + path);
-      console.log(res)
+      const res = await axios.get("/posts/" + path);
+      setPost(res.data)
     };
+    getPost()
   }, [path])
   return (
     <div className='singlePost'>
