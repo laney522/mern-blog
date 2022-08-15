586c7589f32c8e5d3657c508677a9fff464bc0f3
laney522 <pm72222@gmail.com>
2022-08-15 20:35:37 +0900

    update Register.jsx

--- client/src/pages/register/Register.jsx
+++ client/src/pages/register/Register.jsx
@@ -1,30 +1,41 @@
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import "./register.css"
 
 export default function Register() {
+  const [username, setUsername] = useState("")
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
   return (
     <div className='register'>
       <span className="registerTitle">Register</span>
-      <form className="registerForm">
+      <form className="registerForm" onSubmit={handleSubmit}>
         <label>Username</label>
         <input
           type="text"
           className='registerInput'
           placeholder='Enter your username...'
+          onChange={e => setUsername(e.target.value)}
         />
         <label>Email</label>
         <input
           type="text"
           className='registerInput'
           placeholder='Enter your email...'
+          onChange={e => setEmail(e.target.value)}
         />
         <label>Password</label>
         <input
           type="password"
           className='registerInput'
           placeholder='Enter your password...'
+          onChange={e => setPassword(e.target.value)}
         />
-        <button className="registerButton">Register</button>
+        <button className="registerButton" type="submit">Register</button>
       </form>
       <button className="registerLoginButton">
         <Link to='/login' style={{ textDecoration: 'none', color: 'white' }}>Login</Link>
