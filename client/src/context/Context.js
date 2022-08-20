906588bcefca097a6039b010809442c9c098d6dc
laney522 <pm72222@gmail.com>
2022-08-20 22:19:17 +0900

    add initial_state in Context.js

--- client/src/context/Context.js
+++ client/src/context/Context.js
@@ -1,7 +1,13 @@
-const INITIAL STATE = {
+import { createContext, useReducer } from "react";
+
+const INITIAL_STATE = {
   user: null,
   isFetching: false,
   error: false,
 };
 
-export const Context = createContext()
+export const Context = createContext(INITIAL_STATE);
+
+export const ContextProvider = ({children}) => {
+  
+}
