79c18dbbeb5a7ef4f680f53fe934cb3a26053b1c
laney522 <pm72222@gmail.com>
2022-08-21 15:41:52 +0900

    Login.jsx updated

--- client/src/pages/login/Login.jsx
+++ client/src/pages/login/Login.jsx
@@ -1,16 +1,23 @@
-import { Link } from 'react-router-dom';
+import { Link, useHref } from 'react-router-dom';
 import "./login.css"
 
 export default function Login() {
+
+  const userRef = useHref();
+  const passwordRef = useRef();
+
+  const handleSubmit = (e) =>{
+    e.preventDefault()
+  }
   return (
     <div className='login'>
       <span className="loginTitle">Login</span>
-      <form className="loginForm">
+      <form className="loginForm" onSubmit={handleSubmit}>
         <label>Username</label>
         <input type="text" className='loginInput' placeholder='Enter your username...' />
         <label>Password</label>
         <input type="password" className='loginInput' placeholder='Enter your password...' />
-        <button className="loginButton">Login</button>
+        <button className="loginButton" type='submit'>Login</button>
       </form>
       <button className="loginRegisterButton" >
         <Link className="link" to='/register' >Register</Link>
