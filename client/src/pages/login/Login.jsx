a25febbc4b5a0c0ed2e98cc3a9d7185584ef5c1a
laney522 <pm72222@gmail.com>
2022-08-24 23:38:25 +0900

    edited context.js

--- client/src/pages/login/Login.jsx
+++ client/src/pages/login/Login.jsx
@@ -9,7 +9,7 @@ export default function Login() {
 
   const userRef = useRef();
   const passwordRef = useRef();
-  const { dispatch, isFetching } = useContext(Context);
+  const { user, dispatch, isFetching } = useContext(Context);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -25,7 +25,7 @@ export default function Login() {
     }
   };
 
-  console.log(isFetching);
+  console.log(user);
   return (
     <div className='login'>
       <span className="loginTitle">Login</span>
