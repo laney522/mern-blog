3954996b4cdbdbf95bdbeed5cf192083205b9809
laney522 <pm72222@gmail.com>
2022-08-31 23:42:55 +0900

    update SinglePost.jsx

--- client/src/components/singlePost/SinglePost.jsx
+++ client/src/components/singlePost/SinglePost.jsx
@@ -1,6 +1,7 @@
 import axios from "axios";
-import { useEffect, useState } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import { Link, useLocation } from 'react-router-dom'
+import { Context } from '../../context/Context';
 import "./singlePost.css"
 
 export default function SinglePost() {
@@ -8,6 +9,7 @@ export default function SinglePost() {
   const path = location.pathname.split("/")[2];
   const [post, setPost] = useState({});
   const PF = "http://localhost:5000/images/";
+  const { user } = useContext(Context);
 
   useEffect(() => {
     const getPost = async () => {
@@ -25,10 +27,12 @@ export default function SinglePost() {
         )}
         <h1 className="singlePostTitle">
           {post.title}
-          <div className="singlePostEdit">
-            <i className="singlePostIcon far fa-edit"></i>
-            <i className="singlePostIcon fa-solid fa-trash-can"></i>
-          </div>
+          {post.username === user?.username &&
+            <div className="singlePostEdit">
+              <i className="singlePostIcon far fa-edit"></i>
+              <i className="singlePostIcon fa-solid fa-trash-can" onClick={handleDelete}></i>
+            </div>
+          }
         </h1>
         <div className="singlePostInfo">
           <span className='singlePostAuthor'>
